0b420c7540616df57745c3c885d6c61fc4229713
Vcky4 <okon.victor.u@gmail.com>
2022-11-16 15:40:38 +0100

    import images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
-  <V-app :style="{ background: $vuetify.theme.themes.dark.background  }">
+  <V-app :style="{ background: $vuetify.theme.themes.dark.background }">
     <Navbar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+            height="300px"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
   </V-app>
 </template>
 
