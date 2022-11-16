431fcc7f88a82fc054d63cd28f4402906081964a
Vcky4 <okon.victor.u@gmail.com>
2022-11-16 16:38:11 +0100

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
-  <V-app :style="{ background: $vuetify.theme.themes.dark.background }">
+  <V-app id="home" :style="{ background: $vuetify.theme.themes.dark.background }">
     <Navbar />
     <v-container fluid>
       <v-row>
         <v-col cols="6">
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-            height="300px"
-          ></v-img>
+          <v-img src="1.png" contain max-height="500"></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h5 class="red--text text-darken-4 top">Hello I'm Victor</h5>
+          <h1 class="white--text ">Visual Designer</h1>
+          <p class="grey--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quisquam
+            voluptas, quod, quia, voluptates quae voluptatibus quibusdam <br />
+            voluptatum quidem quos natus quas. Quisquam, quae. Quisquam, <br /> quae.
+            Quisquam, quae. Quisquam, quae.
+          </p>
+          <v-btn tile color="#A7121D" dark>About me</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -24,3 +32,72 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.top {
+  margin-top: 180px;
+}
+
+.topInverse {
+  margin-top: -250px;
+}
+
+.topTolbar {
+  margin-top: 100px;
+  text-align: center;
+}
+
+.first {
+  width: 100%;
+  height: 610px;
+  background: linear-gradient(to right,
+      #181818,
+      #181818 50%,
+      #111111 50%,
+      #111111 50%);
+      text-align: center;
+      padding: 2rem 2rem;
+}
+.second{
+  width: 100%;
+  height: 400px;
+  background: #181818;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.secondchild1{
+  display: inline-block;
+  background-color: #1e1e1e;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-top: 250px;
+}
+.child{
+  display: inline-block;
+  background-color: #1e1e1e;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-right: 8px;
+}
+.bgColor1{
+  background-color: #1e1e1e;
+}
+.bgColor2{
+  background-color: #ce1d2a;
+}
+.child1{
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-right: 5px;
+  width: 240px;
+}
+.child2{
+  display: inline-block;
+  vertical-align: middle;
+  width: 245px;
+}
+</style>
